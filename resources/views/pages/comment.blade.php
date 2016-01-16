c623336da7db368532edfc438edeea908c310a77
Hasan Hafiz Pasha <mac_pasha@ovi.com>
2016-01-17 02:06:42 +0600

    Adding comment Section

---
+++ resources/views/pages/comment.blade.php
@@ -0,0 +1,16 @@
+@if(!Auth::check())
+    <p>Please login first</p>
+@else
+    {!! Form::open(['route'=>'comment.create', 'method' => 'post'],array('id' => 'commentform','class' => 'comment-form')) !!}
+    {!! Form::hidden('book_id', 1) !!}
+    <p class="comment-form-comment">
+        {!! Form::label('comment', 'Your Review', ['class' => 'greview']) !!}
+        {!! Form::textarea('comment') !!}
+    </p>
+
+    <p class="form-submit">
+        {!! Form::submit('Post Comment', ['class' => 'submit']) !!}
+    </p>
+
+    {!! Form::close() !!}
+@endif
