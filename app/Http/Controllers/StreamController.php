fa86347ee818d23201e8267913caa98cb5975e46
Hasan Hafiz Pasha <mac_pasha@ovi.com>
2016-02-02 02:11:32 +0600

    Stream Complete

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -11,15 +11,51 @@ class StreamController extends Controller
 {
     public function show()
     {
-        //$users = \DB::table('favorites')->select('user_id', 'book_id', 'created_at')->paginate(15);
-        //return view('pages.stream')->with('users',$users);
 
-        $favoritesStream    =   \DB::table('favorites')->select('user_id', 'book_id', 'created_at')->get();
-        $commentStrem       =   \DB::table('comments')->select('user_id', 'book_id','comment', 'created_at')->get();
-        $followStrem        =   \DB::table('follows')->select('follower_id', 'followed_id', 'created_at')->get();
+        //$favoritesStream    =   \DB::table('favorites')->select('user_id', 'book_id', 'created_at')->get();
+        //$commentStrem       =   \DB::table('comments')->select('user_id', 'book_id','comment', 'created_at')->get();
+        //$followStrem        =   \DB::table('follows')->select('follower_id', 'followed_id', 'created_at')->get();
 
-        $result = array_merge($commentStrem, $followStrem);
-        //return view('pages.test')->with('results',$result);
-        dd($result);
+        if(\Auth::check()){
+            $userID =   \Auth::id();
+            $results = \DB::select( \DB::raw("
+                            SELECT users.name as name, books.title as title, type
+                            FROM users,books,favorites
+                            WHERE favorites.user_id=users.id
+                            AND favorites.book_id=books.id
+                            AND users.id IN (SELECT follows.followed_id FROM follows WHERE follower_id=$userID)")
+            );
+            foreach ($results as $result) {
+                $result =   array($result);
+                $dataArray[]  =   array(
+                    'col1'  =>  $result[0]->name,
+                    'col2'  =>  $result[0]->title,
+                    'col3'  =>  $result[0]->type
+                );
+            }
+
+            $results = \DB::select( \DB::raw("
+                                    SELECT users.name as name, books.title as title, type
+                                    FROM users,books,comments
+                                    WHERE comments.user_id=users.id
+                                    AND comments.book_id=books.id
+                                    AND users.id IN (SELECT follows.followed_id FROM follows WHERE follower_id=$userID)")
+            );
+            foreach ($results as $result) {
+                $result =   array($result);
+                $dataArray[]  =   array(
+                    'col1'  =>  $result[0]->name,
+                    'col2'  =>  $result[0]->title,
+                    'col3'  =>  $result[0]->type
+                );
+            }
+
+
+            return view('pages.stream')->with('items', $dataArray);
+        }
+        else
+            \Session::flash('message', "You Are not to authorized to See this page page, Please Login first");
+            return redirect('error');
+
     }
 }
--- resources/views/pages/stream.blade.php
+++ resources/views/pages/stream.blade.php
@@ -19,31 +19,31 @@
             <div class="Grid__column col-md-8 col-md-offset-2 centered">
                 <ul class="Lesson-List ">
 
-                    @foreach($users as $user)
-                    <li class="Lesson-List__item">
-                        <span class="Lesson-List__status">
-                          <i class="fa fa-check-circle-o material-icons"></i>
-                        </span>
+                    @foreach($items as $item)
+                        <li class="Lesson-List__item">
+                            <span class="Lesson-List__status">
+                              <i class="fa fa-check-circle-o material-icons"></i>
+                            </span>
 
-                        <span class="Lesson-List__title utility-flex">
-                          <a class="green" href="/series/laravel-5-and-the-front-end">
-                              {{ $user->user_id }}
-                          </a> Likes
-                          <a class="green" href="/series/laravel-5-and-the-front-end/episodes/1">
-                              {{ $user->book_id }}
-                          </a>
-                        </span>
-
-                        <span class="Lesson-List__date">
-                            {{ date('F d, Y', strtotime($user->created_at)) }}
-                        </span>
-                    </li>
+                            <span class="Lesson-List__title utility-flex">
+                              <a class="green" href="/series/laravel-5-and-the-front-end">
+                                  {{ $item['col1'] }}
+                              </a>
+                                @if($item['col3'] == 'c')
+                                    Comment
+                                @elseif($item['col3'] == 'follow')
+                                    Follow
+                                @else
+                                    Favorites
+                                @endif
+                                <a class="green" href="/series/laravel-5-and-the-front-end/episodes/1">
+                                    {{ $item['col2'] }}
+                                </a>
+                            </span>
+                        </li>
                     @endforeach
                 </ul>
-                {!! $users->render() !!}
             </div>
         </section>
-
     </div>
 @endsection
-
